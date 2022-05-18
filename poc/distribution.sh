9db522b3c2c864272e6adbb505b442a93357e83f
Jared Still <jkstill@gmail.com>
2022-05-18 11:31:33 -0700

    added tarbal

--- poc/distribution.sh
+++ poc/distribution.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
 declare zipFile=poc-unstable-plans.zip
+declare tarFile=poc-unstable-plans.tgz
 
-rm -f $zipFile
+rm -f $zipFile $tarFile
 
-zip -ry $zipFile  poc-unstable-plans.pl poc-mail-simple.pl *.conf  lib
+zip -ry  $zipFile  poc-unstable-plans.pl poc-mail-simple.pl *.conf  lib
+tar cvfz $tarFile  poc-unstable-plans.pl poc-mail-simple.pl *.conf  lib
 
 echo
 echo "####### $zipFile contents ##############"
 
 unzip -l $zipFile
+
+echo
+echo
+echo "####### $tarFile contents ##############"
+
+tar tvfz $tarFile
+
+echo
 
 
