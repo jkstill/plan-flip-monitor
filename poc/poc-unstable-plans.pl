9a48c5dc853d9e6ff5581dbf287e631470ffdf27
Jared Still <jkstill@gmail.com>
2022-05-17 17:35:54 -0700

    added much documentation

--- poc/poc-unstable-plans.pl
+++ poc/poc-unstable-plans.pl
@@ -8,6 +8,7 @@ use FileHandle;
 use DBI;
 use Getopt::Long;
 use Data::Dumper;
+use Pod::Usage;
 use Time::Local;
 use IO::File;
 use Cwd qw/abs_path/ ;
@@ -98,8 +99,8 @@ Getopt::Long::GetOptions(
 	"username=s" => \$username,
 	"password=s" => \$password,
 	"begin-time=s" => \$snapStartTime,
-  	"min-stddev=n" => \$defMinNormStddev,
-  	"max-exe-time=n" => \$defMinimumMaxEtime,
+  	"min-stddev=f" => \$defMinNormStddev,
+  	"max-exe-time=f" => \$defMinimumMaxEtime,
 	"end-time=s" => \$snapEndTime,
 	"csv!" => \$csvOutput,
 	"csv-delimiter=s" => \$csvDelimiter,
@@ -111,7 +112,10 @@ Getopt::Long::GetOptions(
 	"local-sysdba!",
 	"sysoper!",
 	"debug!" => \$DEBUG,
-	"z|h|help" => \$help );
+	'help!'    => sub { pod2usage( -verbose => 1 ) },
+	'man!'     => sub { pod2usage( -verbose => 2 ) },
+	#"z|h|help" => \$help 
+) or pod2usage( -verbose => 1 );
 
 $localSysdba=$optctl{'local-sysdba'};
 
@@ -347,69 +351,292 @@ format STDOUT =
 ### end of main                ###
 ##################################
 
-sub usage {
-	my $exitVal = shift;
-	$exitVal = 0 unless defined $exitVal;
-	use File::Basename;
-	my $basename = basename($0);
-	print qq{
+=head1 SYNOPSIS
 
-usage: $basename
+ Catch plan flips that have caused a performance degradation
 
-Detect USER SQL where executions are outside the stddev of execution times
-Currently the values to detect these are hardcoded to a low value.
+ This script checks calculated the stddev for SQL execution times.
 
-By default DBA_HIST views are used to look at historical data. 
-'Historical' can be as recent as the most recent snapshot-1, snapshot.
+ Detect USER SQL where executions are outside the stddev of execution times
+ Currently the values to detect these are hardcoded to a low value.
 
-The --realtime option will instead look at realtime data in gv\$sqlstats
+ By default DBA_HIST views are used to look at historical data. 
+ 'Historical' can be as recent as the most recent snapshot-1, snapshot.
 
-The script will report on SQL statements where these criteria are met:
+ The --realtime option will instead look at realtime data in gv\$sqlstats
 
- normalized stddev of execution time is N.N of stddev - default is 0.001
- the maximum execution time is N.N seconds or more - default is 0.001
+ The script will report on SQL statements where these criteria are met:
 
-The defaults will likely catch a few SQL statements.  
+   --min-stddev   normalized stddev of execution time is N.N of stddev - default is 0.001
+   --max-exe-time the maximum execution time is N.N seconds or more - default is 0.001
 
-Using the defaults gets a report that may be used to tune the values for --min-stddev and --min-exe-time
+ The defaults will likely catch a few SQL statements.  
+
+ Using the defaults gets a report that may be used to tune the values for --min-stddev and --min-exe-time
+
+ When the stddev of some executions exceeds a threshold, AND the longest execution time passes a threshold,
+ the SQL and Plan Hash value are reported.
+
+ Optionally, an email can be sent showing the reports and list of sql_id and plan_hash_value.
+
+ examples:
+
+ This command will look through AWR and find user SQL that exceeds the thresholds
+
+ $ $ORACLE_HOME/perl/bin/perl ./poc-unstable-plans.pl --database //rac-scan/swingbench --username somedba --password XXX --begin-time '2022-05-10 00:00:00' --end-time '2022-05-12 23:00:00'  --min-stddev  20
+ Major/Minor version 19/0
+
+ running query for unstable-plans-baseline-historic-CONTAINER
+
+  format: yyyy-mm-dd hh24:mi:ss
+   start: 2022-05-10 00:00:00
+     end: 2022-05-12 23:00:00
+ Epoch: 1652827438
+                                                                          PLAN
+ SQL_ID        PLAN HASH           EXECS USERNAME               AVG_LIO   COUNT        MIN_ETIME        MAX_ETIME     STDDEV_ETIME      NORM_STDDEV
+ ------------- ------------ ------------ --------------- -------------- ------- ---------------- ---------------- ---------------- ----------------
+ 56pwkjspvmg3h   1448083145        59561 SOE                    138.569       2        4.7552763        4.7552763      122.1845768       25.6945275
+ gkxxkghxubh1a   2220165490        59234 SOE                    139.322       2        4.7165040        4.7165040      199.9986083       42.4039942
+ 7hk2m2702ua0g   2307454521           11 SOE                    160.964       6        1.1280896        1.1280896       58.5730151       51.9223059
+ 29qp10usqkqh0   1055577880       119025 SOE                      3.526       3        0.1557687        0.1557687      168.6800624     1082.8879105
+ 8zz6y2yzdqjp0    574689976       595704 SOE                      0.004       2        0.0169154        0.0169154       31.9432872     1888.4093678
+ 7hk2m2702ua0g   2048963432       294374 SOE                      0.010       6        0.0280221        0.0280221       58.5730151     2090.2412237
+ 7ws837zynp1zv   3722429161      2382907 SOE                      0.000       2        0.0050973        0.0050973       20.4907658     4019.9639761
+ 7t0959msvyt5g    856749079       894670 SOE                      0.003       2        0.0122687        0.0122687       75.2371300     6132.4693216
+ 1qf3b7a46jm3u   1322380957       894727 SOE                      0.001       2        0.0094323        0.0094323       78.7726617     8351.3943886
+ g81cbrq5yamf5   2480532011      3278664 SOE                      0.000       2        0.0062281        0.0062281       55.1291887     8851.6471375
 
 
-  --database      target instance
-  --username      target instance account name
-  --password      target instance account password
-  --sysdba        logon as sysdba
-  --csv           switch to CSV output
-  --begin-time    earliest time to check AWR, in 'YYYY-MM-DD HH24:MI:SS' format
-  --end-time      latest time to check AWR, in 'YYYY-MM-DD HH24:MI:SS' format
-  --realtime      look at realtime data in gv\$sqlstats.
-                  the --begin-time and --end-time arguments are ignored
-  --min-stddev    minimum value of normalized stddev exe times to look for - defaults to 0.001
-  --max-exe-time  minimum value of max execution time to look for  - defaults to 0.001
-  --send-alerts   send alert emails - default is to not send alerts
-  --save-alerts   save alert history - default is to not save alert history
-                  when used with --send-alerts and --alert-freq it is used to control when alerts are sent
-  --alert-freq    how many seconds until the next alert is sent for a SQL_ID. default is 86400
-  --sysoper       logon as sysoper
-  --local-sysdba  logon to local instance as sysdba. ORACLE_SID must be set
-                  the following options will be ignored:
-                   --database
-                   --username
-                   --password
+ The previous command used the '--min-stddev 20' argument to limit the SQL found.
 
-  examples:
+ The '-max-exe-time 3' option is now used to show only those SQL statements that at some time in this period had an elapsed time exceeding 3 seconds.
 
-  Remote SYSDBA
-  \$ORACLE_HOME/perl/bin/perl  $basename  -database someserver/orcl.yourdomain.com -sysdba -username sys -password XXXX -begin-time '2022-05-04 00:00:00' -end-time '2022-05-04 08:00:00'
+ $ $ORACLE_HOME/perl/bin/perl ./poc-unstable-plans.pl --database //rac-scan/swingbench --username somedba --password XXX --begin-time '2022-05-10 00:00:00' --end-time '2022-05-12 23:00:00'  --min-stddev  20 --max-exe-time 3
+ Major/Minor version 19/0
 
-  Remote DBA
-  \$ORACLE_HOME/perl/bin/perl  $basename  -database someserver/orcl.yourdomain.com -sysdba -username dbauser -password XXXX -begin-time '2022-05-04 00:00:00' -end-time '2022-05-04 08:00:00'
+  running query for unstable-plans-baseline-historic-CONTAINER
 
-  Local SYSDBA
-  \$ORACLE_HOME/perl/bin/perl  $basename  -database someserver/orcl.yourdomain.com -local-sysdba  -begin-time '2022-05-04 00:00:00' -end-time '2022-05-04 08:00:00'
+   format: yyyy-mm-dd hh24:mi:ss
+    start: 2022-05-10 00:00:00
+      end: 2022-05-12 23:00:00
+ Epoch: 1652830981
+                                                                           PLAN
+ SQL_ID        PLAN HASH           EXECS USERNAME               AVG_LIO   COUNT        MIN_ETIME        MAX_ETIME     STDDEV_ETIME      NORM_STDDEV
+ ------------- ------------ ------------ --------------- -------------- ------- ---------------- ---------------- ---------------- ----------------
+ 56pwkjspvmg3h   1448083145        59561 SOE                    138.569       2        4.7552763        4.7552763      122.1845768       25.6945275
+ gkxxkghxubh1a   2220165490        59234 SOE                    139.322       2        4.7165040        4.7165040      199.9986083       42.4039942
+ 
 
-};
-   exit $exitVal;
-};
+ Should you find a period that represents good perfomance, you may use the --save-alerts option.
+
+ Doing so will save the sql_id, plan_hash_value, and the current time to poc-plan-flip.csv file.
+
+ $ $ORACLE_HOME/perl/bin/perl ./poc-unstable-plans.pl --database //rac-scan/swingbench --username somedba --password XXX --begin-time '2022-05-10 00:00:00' --end-time '2022-05-12 23:00:00'  --min-stddev  20 --max-exe-time 3
+ Major/Minor version 19/0
+
+  running query for unstable-plans-baseline-historic-CONTAINER
+
+   format: yyyy-mm-dd hh24:mi:ss
+    start: 2022-05-10 00:00:00
+      end: 2022-05-12 23:00:00
+ Epoch: 1652831362
+                                                                           PLAN
+ SQL_ID        PLAN HASH           EXECS USERNAME               AVG_LIO   COUNT        MIN_ETIME        MAX_ETIME     STDDEV_ETIME      NORM_STDDEV
+ ------------- ------------ ------------ --------------- -------------- ------- ---------------- ---------------- ---------------- ----------------
+ 56pwkjspvmg3h   1448083145        59561 SOE                    138.569       2        4.7552763        4.7552763      122.1845768       25.6945275
+ gkxxkghxubh1a   2220165490        59234 SOE                    139.322       2        4.7165040        4.7165040      199.9986083       42.4039942
+
+ $  cat poc-plan-flip.csv
+ 56pwkjspvmg3h,1448083145,1652831362
+ gkxxkghxubh1a,2220165490,1652831362
+
+ The poc-plan-flip.csv is used when the --send-alerts option is used. 
+
+ If the time recorded in 'poc-plan-flip.csv' is less than the current (epoch) time - 86400, an alert will not be sent.
+
+ The amount of time before sending an alert again can be altered with the '--alert-freq' option.
+
+ The '--realtime' option can now be used to look for SQL statements whose execution time has become excessive.
+
+ $ $ORACLE_HOME/perl/bin/perl ./poc-unstable-plans.pl --realtime --database //rac-scan/swingbench --username somedba --password XXX  --min-stddev  20 --max-exe-time 3 --save-alerts --send-alerts 
+ 
+ $ $ORACLE_HOME/perl/bin/perl ./poc-unstable-plans.pl --realtime --database //rac-scan/swingbench --username jkstill --password XXX realtime --send-alerts --save-alerts  --min-stddev 0.05
+ Major/Minor version 19/0
+
+
+  running query for unstable-plans-baseline-realtime-CONTAINER
+ Epoch: 1652827081
+                                                                           PLAN
+ SQL_ID        PLAN HASH           EXECS USERNAME               AVG_LIO   COUNT        MIN_ETIME        MAX_ETIME     STDDEV_ETIME      NORM_STDDEV
+ ------------- ------------ ------------ --------------- -------------- ------- ---------------- ---------------- ---------------- ----------------
+ 29qp10usqkqh0   3619984409            1 SOE                 436363.000       2      244.2520940      244.2520940       17.5796194        0.0719733
+ 29qp10usqkqh0    388976350            9 SOE                 123335.605       2      219.3907579      219.3907579       17.5796194        0.0801293
+ Sending alerts!
+
+ The next time the script is run, it will not alert:
+  
+ $ $ORACLE_HOME/perl/bin/perl ./poc-unstable-plans.pl --realtime --database //rac-scan/swingbench --username jkstill --password XXX realtime --send-alerts --save-alerts  --min-stddev 0.05
+ Major/Minor version 19/0
+
+
+  running query for unstable-plans-baseline-realtime-CONTAINER
+ Epoch: 1652827112
+
+
+ Alerts can be forced by using '--alert-freq 0'. This also causes the alert time to be updated in the 'poc-plan-flip.csv'.
+
+ $ $ORACLE_HOME/perl/bin/perl ./poc-unstable-plans.pl --alert-freq 0 --realtime --database //rac-scan/swingbench --username jkstill --password XXX realtime --send-alerts --save-alerts  --min-stddev 0.05
+ Major/Minor version 19/0
+
+
+  running query for unstable-plans-baseline-realtime-CONTAINER
+ Epoch: 1652827256
+
+                                                                           PLAN
+ SQL_ID        PLAN HASH           EXECS USERNAME               AVG_LIO   COUNT        MIN_ETIME        MAX_ETIME     STDDEV_ETIME      NORM_STDDEV
+ ------------- ------------ ------------ --------------- -------------- ------- ---------------- ---------------- ---------------- ----------------
+ 29qp10usqkqh0   3619984409            1 SOE                 436363.000       2      244.2520940      244.2520940       17.5796194        0.0719733
+ 29qp10usqkqh0    388976350            9 SOE                 123335.605       2      219.3907579      219.3907579       17.5796194        0.0801293
+ Sending alerts!
+
+
+=head1 OPTIONS
+
+=over
+
+
+=item --database target instance
+
+ set the target instance to connect to
+
+=item --username
+
+ target instance account name
+
+
+=item --password
+
+ target instance account password
+
+
+=item --sysdba
+	 
+ logon as sysdba
+
+
+=item --csv
+
+ switch to CSV output
+
+
+=item --begin-time
+
+ earliest time to check AWR, in 'YYYY-MM-DD HH24:MI:SS' format
+
+
+=item --end-time
+
+ latest time to check AWR, in 'YYYY-MM-DD HH24:MI:SS' format
+
+
+=item --realtime
+
+ look at realtime data in gv$sqlstats.
+ the --begin-time and --end-time arguments are ignored
+
+
+=item --min-stddev
+
+minimum value of normalized stddev exe times to look for - defaults to 0.001
+
+=item --max-exe-time
+
+minimum value of max execution time to look for  - defaults to 0.001
+
+=item --send-alerts
+
+send alert emails - default is to not send alerts
+
+=item --save-alerts
+
+save alert history - default is to not save alert history
+when used with --send-alerts and --alert-freq it is used to control when alerts are sent
+
+
+=item --alert-freq
+
+ how many seconds until the next alert is sent for a SQL_ID. 
+ default is 86400
+
+=item --sysoper
+
+ logon as sysoper
+
+=item --local-sysdba
+
+ logon to local instance as sysdba. ORACLE_SID must be set
+
+ the following options will be ignored:
+   --database
+   --username
+   --password
+
+=back
+
+=head1 Config file 'planflip.conf'
+
+ The optional configuration file is 'planflip.conf'.
+
+ This is really just Perl code that is evaluated at runtime.
+
+ Any values found in the config file will override built-in defaults.
+
+ Values entered via command line option will override defaults as well as the values in the config file.
+
+ Example configfile 'planflip.conf':
+
+   # this is really just perl code that is read at runtime.
+   # this is safe in this context, that is, anyone with access to this config file
+   # also has access to the perl script
+
+   # call this after the variables have been declared in the main script
+   #
+   %emailConfig = (
+
+	   mailfrom => 'oracle@yourdomain.com',
+	   mailto => 'somedba-01@yourdomain.com,somedba-02@yourdomain.com',
+	   mailsubject => 'unstable plans report',
+	   mailmsg => 'this is the body of the email and will be replaced programmatically',
+
+   );
+
+   # only change these if necessary, as they will override the defaults
+   # the command line options will override the settings below here
+
+
+   #$csvOutput=1;
+   #$csvDelimiter='|';
+   #$timeScope='realtime';
+   #$realtime=1;
+   #$defMinNormStddev=0.010;
+   #$defMinimumMaxEtime=0.5;
+
+=head1 Sending Mail
+
+ It is assumed the host machine has smtp or ssmtp configured and it is available to the user running this script.
+
+ If that is not the case, then the '--send-alerts' option will not work properly.
+
+=head1 Suggested Strategy
+
+ * find a period that represents decent performance with the --begin-time and --end-time options
+ * use the --save-alerts option
+ * use cron or a scheduler to run with these options
+  --realtime
+  --save-alerts
+  --send-alerts
+
+=cut
 
 sub getDbType {
 	my ($dbh,$majorOraVersion) = @_;
